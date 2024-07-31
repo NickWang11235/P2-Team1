c63d3c26e869e7d06e43aadf15daa1f3b8946ce9
Ned <nedturner613@gmail.com>
2024-07-31 16:27:10 -0500

    struggling with html

---
+++ Bank-FrontEnd/main.js
@@ -0,0 +1,15 @@
+const form = document.querySelector('form');
+let output = document.querySelector('#output');
+
+const userLogin = document.getElementById('login-form');
+const userLoginSubmit = document.getElementById('submit-button');
+
+
+
+
+userLoginSubmit.addEventListener('submit', function(){
+    window.location.href = './account-info.html';
+    // location.href = './account-info.html';
+})
+
+
