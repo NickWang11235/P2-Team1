782358578d973b6636d14dfaaf0b9fa3e9d21c11
nick wang <nickwang11235@gmail.com>
2024-08-02 13:34:57 -0700

    i only added read operation and forgot rest of the CRUD :(. Now added the rest of CRUD

--- Bank-Backend/Program.cs
+++ Bank-Backend/Program.cs
@@ -1,17 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<BankContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BankDB")));
-builder.Services.AddScoped<IBankRepository>(repo => new BankRepository(builder.Configuration.GetConnectionString("BankDB") ?? ""));
+public class Program
+{
+    public static void Main(string[] args)
+    {
 
-var app = builder.Build();
-app.MapGet("/", () => "Hello World!");
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddDbContext<BankContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BankDB")));
+        builder.Services.AddScoped<IBankRepository>(repo => new BankRepository(builder.Configuration.GetConnectionString("BankDB") ?? ""));
 
-app.Run();
-// public class Program
-// {
-//     public static void Main(string[] args)
-//     {
+        var app = builder.Build();
+        app.MapGet("/", () => "Hello World!");
 
-//     }
-// }
+        app.Run();
+    }
+}
